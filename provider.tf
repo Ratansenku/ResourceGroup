64fb035266104385bd64e233c00e93514b842be2
ratansen kumar <ratansenkumar@gmail.com>
2025-03-14 18:17:28 +0530

    code

--- provider.tf
+++ provider.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.89.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {
-
-  }
+  features {}
   subscription_id = "1fe49dcb-216b-4a9c-a412-afa7742cee3d"
 }
